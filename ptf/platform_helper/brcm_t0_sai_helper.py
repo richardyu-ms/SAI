bf631ea3c8c223064953210e7dd2d1602de5311b
Yang Wang <yangwang1@microsoft.com>
2022-05-26 09:36:59 +0000

    add port channel config

--- ptf/platform_helper/brcm_t0_sai_helper.py
+++ ptf/platform_helper/brcm_t0_sai_helper.py
@@ -29,6 +29,23 @@ DEFAULT_IP_V6_PREFIX = '0000:0000:0000:0000:0000:0000:0000:0000'
 LOCAL_IP_128V6_PREFIX = 'fe80::f68e:38ff:fe16:bc75/128'
 LOCAL_IP_10V6_PREFIX = 'fe80::/10'
 PORT_MTU=9122
+
+PC_IP_32V4_PREFIX1 = '10.0.0.56/32'
+PC_IP_32V4_PREFIX2 = '10.0.0.58/32'
+PC_IP_32V4_PREFIX3 = '10.0.0.60/32'
+PC_IP_32V4_PREFIX4 = '10.0.0.62/32'
+
+PC_IP_128V6_PREFIX1 = 'fc00::71/128'
+PC_IP_128V6_PREFIX2 = 'fc00::75/128'
+PC_IP_128V6_PREFIX3 = 'fc00::79/128'
+PC_IP_128V6_PREFIX4 = 'fc00::7d/128'
+PC_PORT_MTU = 9100
+NAT_ZONE_ID = 0
+
+POLICER_CBS1 = 600
+POLICER_CIR1 = 600
+POLICER_CBS2 = 6000
+POLICER_CIR2 = 6000
 class BrcmT0SaiHelper(CommonSaiHelper):
     """
     This class contains broadcom(brcm) specified functions for the platform setup and test context configuration.
@@ -271,6 +288,485 @@ class BrcmT0SaiHelper(CommonSaiHelper):
             sai_thrift_set_port_attribute(
                 self.client, port_oid=port, mtu=PORT_MTU, admin_state=True)
 
+
+    def config_portchannel(self):
+        """
+        Default configration for Port Channel 
+        """
+        print("Get Port List...")
+        hw_port1 = 0
+        hw_port2 = 1
+        hw_port3 = 2
+        hw_port4 = 3
+
+        port_list = sai_thrift_object_list_t(count=100)
+        p_list = sai_thrift_get_switch_attribute(
+            self.client, port_list=port_list)
+        self.port_list = p_list['port_list'].idlist
+
+        self.port1 = self.port_list[hw_port1]
+        self.port2 = self.port_list[hw_port2]
+        self.port3 = self.port_list[hw_port3]
+        self.port4 = self.port_list[hw_port4]
+
+        print("Create lag...")
+        self.vlan_id1 = 1
+        self.vlan_id2 = 1000
+        self.lag_list = []
+
+        self.lag_id1 = sai_thrift_create_lag(self.client)
+        self.lag_id2 = sai_thrift_create_lag(self.client)
+        self.lag_id3 = sai_thrift_create_lag(self.client)
+        self.lag_id4 = sai_thrift_create_lag(self.client)
+
+        sai_thrift_set_port_attribute(self.client, port_oid=self.port1, port_vlan_id=self.vlan_id1)
+        self.lag_member1 = sai_thrift_create_lag_member(
+            self.client, 
+            lag_id=self.lag_id1, 
+            port_id=self.port1,
+            egress_disable=True,
+            ingress_disable=True)
+
+        sai_thrift_set_port_attribute(self.client, port_oid=self.port2, port_vlan_id=self.vlan_id1)
+        self.lag_member2 = sai_thrift_create_lag_member(
+            self.client, 
+            lag_id=self.lag_id2, 
+            port_id=self.port2,
+            egress_disable=True,
+            ingress_disable=True)
+
+        sai_thrift_set_port_attribute(self.client, port_oid=self.port3, port_vlan_id=self.vlan_id1)
+        self.lag_member3 = sai_thrift_create_lag_member(
+            self.client, 
+            lag_id=self.lag_id3, 
+            port_id=self.port3,
+            egress_disable=True,
+            ingress_disable=True)
+
+        sai_thrift_set_port_attribute(self.client, port_oid=self.port4, port_vlan_id=self.vlan_id1)
+        self.lag_member4 = sai_thrift_create_lag_member(
+            self.client, 
+            lag_id=self.lag_id4, 
+            port_id=self.port4,
+            egress_disable=True,
+            ingress_disable=True)
+
+        print("Create route...")
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(LOCAL_IP_10V6_PREFIX),
+            switch_id=self.switch_id)
+        self.local_10v6_route_entry = sai_thrift_create_route_entry(
+            self.client, route_entry=entry, packet_action=SAI_PACKET_ACTION_FORWARD)
+
+        rif_id1 = sai_thrift_create_router_interface(self.client, 
+            src_mac_address=ROUTER_MAC,
+            type=SAI_ROUTER_INTERFACE_TYPE_PORT,
+            mtu=PC_PORT_MTU,
+            port_id=self.port1,
+            nat_zone_id=NAT_ZONE_ID)
+        self.assertEqual(self.status(), SAI_STATUS_SUCCESS)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_32V4_PREFIX1),
+            switch_id=self.switch_id)
+
+        self.pc_32v4_route_entry1 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_128V6_PREFIX1),
+            switch_id=self.switch_id)
+            
+        self.pc_128v6_route_entry1 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+        rif_id2 = sai_thrift_create_router_interface(self.client, 
+            src_mac_address=ROUTER_MAC,
+            type=SAI_ROUTER_INTERFACE_TYPE_PORT,
+            mtu=PC_PORT_MTU,
+            port_id=self.port2,
+            nat_zone_id=NAT_ZONE_ID)
+        self.assertEqual(self.status(), SAI_STATUS_SUCCESS)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_32V4_PREFIX2),
+            switch_id=self.switch_id)
+
+        self.pc_32v4_route_entry2 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_128V6_PREFIX2),
+            switch_id=self.switch_id)
+        self.pc_128v6_route_entry2 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+
+        rif_id3 = sai_thrift_create_router_interface(self.client, 
+            src_mac_address=ROUTER_MAC,
+            type=SAI_ROUTER_INTERFACE_TYPE_PORT,
+            mtu=PC_PORT_MTU,
+            port_id=self.port3,
+            nat_zone_id=NAT_ZONE_ID)
+        self.assertEqual(self.status(), SAI_STATUS_SUCCESS)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_32V4_PREFIX3),
+            switch_id=self.switch_id)
+
+        self.pc_32v4_route_entry3 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_128V6_PREFIX3),
+            switch_id=self.switch_id)
+        self.pc_128v6_route_entry3 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+
+        rif_id4 = sai_thrift_create_router_interface(self.client, 
+            src_mac_address=ROUTER_MAC,
+            type=SAI_ROUTER_INTERFACE_TYPE_PORT,
+            mtu=PC_PORT_MTU,
+            port_id=self.port4,
+            nat_zone_id=NAT_ZONE_ID)
+        self.assertEqual(self.status(), SAI_STATUS_SUCCESS)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_32V4_PREFIX4),
+            switch_id=self.switch_id)
+
+        self.pc_32v4_route_entry4 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+
+        entry = sai_thrift_route_entry_t(
+            vr_id=self.default_vrf,
+            destination=sai_ipprefix(PC_IP_128V6_PREFIX4),
+            switch_id=self.switch_id)
+        self.pc_128v6_route_entry4 = sai_thrift_create_route_entry(
+            self.client,
+            route_entry=entry, 
+            packet_action=SAI_PACKET_ACTION_FORWARD)
+    
+
+        print("Create policer...")
+
+        print("Set default trap group...")
+        def_attr = sai_thrift_get_switch_attribute(
+            self.client, default_trap_group=True)
+        self.default_trap_group = def_attr['default_trap_group']
+
+        sai_policer_id1 = sai_thrift_create_policer(self.client,
+                            meter_type=SAI_METER_TYPE_PACKETS,
+                            mode=SAI_POLICER_MODE_SR_TCM,
+                            cbs=POLICER_CBS1,
+                            cir=POLICER_CIR1,
+                            red_packet_action=SAI_PACKET_ACTION_DROP)
+
+        sai_thrift_set_hostif_trap_group_attribute(self.client, hostif_trap_group_oid=self.default_trap_group, queue=0, policer=sai_policer_id1)
+
+        trap_group_id1 = sai_thrift_create_hostif_trap_group(self.client, queue=0)
+
+        sai_policer_id2 = sai_thrift_create_policer(self.client,
+                            meter_type=SAI_METER_TYPE_PACKETS,
+                            mode=SAI_POLICER_MODE_SR_TCM,
+                            cbs=POLICER_CBS2,
+                            cir=POLICER_CIR2,
+                            red_packet_action=SAI_PACKET_ACTION_DROP)
+
+        sai_thrift_set_hostif_trap_group_attribute(self.client, hostif_trap_group_oid=trap_group_id1, policer=sai_policer_id2)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_IP2ME,
+            trap_group=trap_group_id1,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=1)
+
+        trap_group_id2 = sai_thrift_create_hostif_trap_group(self.client, queue=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_BGP,
+            trap_group=trap_group_id2,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_BGPV6,
+            trap_group=trap_group_id2,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_LACP,
+            trap_group=trap_group_id2,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+        
+
+        trap_group_id3 = sai_thrift_create_hostif_trap_group(self.client, queue=4)
+        sai_policer_id3 = sai_thrift_create_policer(self.client,
+                            meter_type=SAI_METER_TYPE_PACKETS,
+                            mode=SAI_POLICER_MODE_SR_TCM,
+                            cbs=POLICER_CBS1,
+                            cir=POLICER_CIR1,
+                            red_packet_action=SAI_PACKET_ACTION_DROP)
+
+        sai_thrift_set_hostif_trap_group_attribute(self.client, hostif_trap_group_oid=trap_group_id3, policer=sai_policer_id3)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_ARP_REQUEST,
+            trap_group=trap_group_id3,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_ARP_RESPONSE,
+            trap_group=trap_group_id3,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_IPV6_NEIGHBOR_DISCOVERY,
+            trap_group=trap_group_id3,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        trap_group_id4 = sai_thrift_create_hostif_trap_group(self.client, queue=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_DHCP,
+            trap_group=trap_group_id4,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_DHCPV6,
+            trap_group=trap_group_id4,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_LLDP,
+            trap_group=trap_group_id4,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+        sai_thrift_create_hostif_trap(self.client,
+            trap_type=SAI_HOSTIF_TRAP_TYPE_UDLD,
+            trap_group=trap_group_id4,
+            packet_action=SAI_PACKET_ACTION_TRAP,
+            trap_priority=4)
+
+
+        print("Disable Lag configration...")
+        sai_thrift_set_port_attribute(self.client, port_oid=self.port1, port_vlan_id=self.vlan_id1)
+
+        sai_thrift_set_lag_member_attribute(
+            self.client,
+            lag_member_oid=self.lag_member1,
+            egress_disable=False,
+            ingress_disable=False
+        )
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id1,
+            ip_address=sai_ipaddress('10.0.0.57'),
+            switch_id=self.switch_id,
+            )
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='02:D9:B8:E2:36:33'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('10.0.0.57'),
+            router_interface_id=rif_id1,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+        sai_thrift_set_port_attribute(self.client, port_oid=self.port4, port_vlan_id=self.vlan_id1)
+
+        sai_thrift_set_lag_member_attribute(
+            self.client,
+            lag_member_oid=self.lag_member1,
+            egress_disable=False,
+            ingress_disable=False
+        )
+
+        rif_id2 = sai_thrift_create_router_interface(self.client, 
+            src_mac_address=ROUTER_MAC,
+            type=SAI_ROUTER_INTERFACE_TYPE_PORT,
+            mtu=PC_PORT_MTU,
+            port_id=self.port2,
+            nat_zone_id=NAT_ZONE_ID)
+            
+        sai_thrift_set_router_interface_attribute(self.client, router_interface_oid=rif_id1, mtu=PC_PORT_MTU)
+        sai_thrift_set_router_interface_attribute(self.client, router_interface_oid=rif_id2, mtu=PC_PORT_MTU)
+        sai_thrift_set_router_interface_attribute(self.client, router_interface_oid=rif_id3, mtu=PC_PORT_MTU)
+        sai_thrift_set_router_interface_attribute(self.client, router_interface_oid=rif_id4, mtu=PC_PORT_MTU)
+
+        sai_thrift_set_lag_member_attribute(self.client, lag_member_oid=self.lag_member2, egress_disable=False, ingress_disable=False)
+        sai_thrift_set_lag_member_attribute(self.client, lag_member_oid=self.lag_member3, egress_disable=False, ingress_disable=False)
+        sai_thrift_set_lag_member_attribute(self.client, lag_member_oid=self.lag_member4, egress_disable=False, ingress_disable=False)
+
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id2,
+            ip_address=sai_ipaddress('10.0.0.59'),
+            switch_id=self.switch_id,
+            )
+
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='32:A3:B0:44:01:DA'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('10.0.0.59'),
+            router_interface_id=rif_id2,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id3,
+            ip_address=sai_ipaddress('10.0.0.61'),
+            switch_id=self.switch_id,
+
+            )
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='4E:8E:DF:55:5F:26'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('10.0.0.61'),
+            router_interface_id=rif_id4,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id4,
+            ip_address=sai_ipaddress('10.0.0.63'),
+            switch_id=self.switch_id,
+            )
+
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='46:B1:78:24:6A:0E'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('10.0.0.63'),
+            router_interface_id=rif_id3,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id1,
+            ip_address=sai_ipaddress('fc00::72'),
+            switch_id=self.switch_id,
+            )
+
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='02:D9:B8:E2:36:33'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('fc00::72'),
+            router_interface_id=rif_id1,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id2,
+            ip_address=sai_ipaddress('fc00::7a'),
+            switch_id=self.switch_id,
+
+            )
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='4E:8E:DF:55:5F:26'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('fc00::7a'),
+            router_interface_id=rif_id2,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id4,
+            ip_address=sai_ipaddress('fc00::7a'),
+            switch_id=self.switch_id,
+            )
+
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='46:B1:78:24:6A:0E'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('fc00::7a'),
+            router_interface_id=rif_id3,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+        nbr_entry = sai_thrift_neighbor_entry_t(
+            rif_id=rif_id4,
+            ip_address=sai_ipaddress('fc00::7a'),
+            switch_id=self.switch_id,
+            )
+
+        status = sai_thrift_create_neighbor_entry(
+            self.client,
+            nbr_entry,
+            dst_mac_address='46:B1:78:24:6A:0E'
+            )
+
+        nexthop = sai_thrift_create_next_hop(
+            self.client,
+            ip=sai_ipaddress('fc00::7a'),
+            router_interface_id=rif_id3,
+            type=SAI_NEXT_HOP_TYPE_IP)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
 
     def set_port_serdes(self):
         """
